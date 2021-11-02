c5ecd9b8dadee2fc3607e1e87d9db66dd7a767c1
Matyas Heins <32817933+matheins@users.noreply.github.com>
2021-11-02 19:20:43 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,46 @@
+import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { AuthModule } from './resolvers/auth/auth.module';
+import { UserModule } from './resolvers/user/user.module';
+import { PostModule } from './resolvers/post/post.module';
+import { AppResolver } from './resolvers/app.resolver';
+import { DateScalar } from './common/scalars/date.scalar';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './configs/config';
+import { GraphqlConfig } from './configs/config.interface';
+import { PrismaModule } from 'nestjs-prisma';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const graphqlConfig = configService.get<GraphqlConfig>('graphql');
+        return {
+          installSubscriptionHandlers: true,
+          buildSchemaOptions: {
+            numberScalarMode: 'integer',
+          },
+          sortSchema: graphqlConfig.sortSchema,
+          autoSchemaFile:
+            graphqlConfig.schemaDestination || './src/schema.graphql',
+          debug: graphqlConfig.debug,
+          playground: graphqlConfig.playgroundEnabled,
+          context: ({ req }) => ({ req }),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    PostModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver, DateScalar],
+})
+export class AppModule {}
